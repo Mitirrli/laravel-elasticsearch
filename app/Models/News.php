6386958414100d5b1c18aaa7bf6b6d73cfa5ca3d
huanglei <huanglei@quanjingshuju.com>
2020-07-02 22:53:02 +0800

    更新model配置

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,8 +5,29 @@ namespace App\Models;
 use Dcat\Admin\Traits\HasDateTimeFormatter;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class News extends Model
 {
-	use HasDateTimeFormatter;    
+	use HasDateTimeFormatter;
+	use Searchable;
+
+    protected $guarded = [];
+
+    public function searchableAs()
+    {
+        return '_doc';
+    }
+
+    // 定义有那些字段需要搜索
+    public function toSearchableArray()
+    {
+        return [
+            'news_id' => $this->id,
+            'news_title' => $this->title,
+            'news_text' => $this->text,
+            'news_created_at' => $this->created_at,
+            'news_updated_at' => $this->updated_at,
+        ];
+    }
 }
