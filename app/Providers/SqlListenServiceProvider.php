5d5e4c4b4cb358fc49b96548335755c66131658b
huanglei <huanglei@quanjingshuju.com>
2020-07-06 13:54:19 +0800

    add sql-debug

--- app/Providers/SqlListenServiceProvider.php
+++ app/Providers/SqlListenServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
@@ -23,13 +24,15 @@ class SqlListenServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $request)
     {
         if (env('APP_ENV') === 'local') {
-            DB::listen(function ($query) {
-                $sql = vsprintf(str_replace('?', '%s', $query->sql) . ', time: '.$query->time, $query->bindings);
+            DB::listen(function ($query) use ($request) {
+                if($request->has('sql-debug')){
+                    $sql = vsprintf(str_replace('?', '%s', $query->sql) . ', time: ' . $query->time, $query->bindings);
 
-                Log::debug($sql);
+                    Log::debug($sql);
+                }
             });
         }
     }
