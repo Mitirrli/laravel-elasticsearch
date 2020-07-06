f4dc620f7c06bd19db36c637ca356e81aa4e4f74
huanglei <huanglei@quanjingshuju.com>
2020-07-06 13:36:49 +0800

    获取sql语句

---
+++ app/Providers/SqlListenServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+class SqlListenServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (env('APP_ENV') === 'local') {
+            DB::listen(function ($query) {
+                $sql = vsprintf(str_replace('?', '%s', $query->sql), $query->bindings);
+
+                //写入日志
+            });
+        }
+    }
+}
