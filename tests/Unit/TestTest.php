b9a425a0f87bd1e2a1c3b6fd26eae6e7a40e1cbd
huanglei <huanglei@quanjingshuju.com>
2020-07-08 10:51:25 +0800

    增加单元测试

--- tests/Unit/TestTest.php
+++ tests/Unit/TestTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Services\CalculateService;
 use PHPUnit\Framework\TestCase;
 
 class TestTest extends TestCase
@@ -14,5 +15,25 @@ class TestTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * Add function.
+     */
+    public function testAdd()
+    {
+        $calculateService = new CalculateService;
+
+        $this->assertEquals(6, $calculateService->add(2, 4));
+    }
+
+    /**
+     * Reduce function.
+     */
+    public function testReduce()
+    {
+        $calculateService = new CalculateService;
+
+        $this->assertEquals(-2, $calculateService->reduce(2, 4));
     }
 }
